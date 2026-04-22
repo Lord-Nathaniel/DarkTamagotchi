716d88b3df9c0293c430424d0e67d1ec96415eb4
Adrien Lozano <ad_lozano@outlook.com>
2026-04-22 16:19:46 +0200

    fix(GameKeyManager):  switch to new input system and refactor

---
+++ Assets/Scripts/GameKeyManager.cs
@@ -0,0 +1,87 @@
+using UnityEngine;
+
+public class GameKeyManager : MonoBehaviour
+{
+    [SerializeField] int inputReqToChangeCamera = 3;
+    [SerializeField] float maxTimeToChangeCamera = 4f;
+
+    private float time = 0f;
+    private int currentInputNb = 0;
+    private DirectionType currentInputDirection;
+
+    // Needed services
+    private InputManager inputManager;
+    private CameraManager cameraManager;
+    private ButtonManager buttonManager;
+
+    private void Awake()
+    {
+        ServiceManager.Register(this);
+    }
+
+    private void OnDestroy()
+    {
+        ServiceManager.Unregister<GameKeyManager>();
+
+        inputManager.OnNavigateDirection -= SwitchCameraByNaviRgationDirection;
+    }
+
+    private void Start()
+    {
+        inputManager = ServiceManager.Get<InputManager>();
+        cameraManager = ServiceManager.Get<CameraManager>();
+        buttonManager = ServiceManager.Get<ButtonManager>();
+
+
+        inputManager = ServiceManager.Get<InputManager>();
+        inputManager.OnNavigateDirection += SwitchCameraByNaviRgationDirection;
+    }
+
+    private void Update()
+    {
+        if (currentInputNb > 0)
+        {
+            time += Time.deltaTime;
+        }
+
+        if (time > maxTimeToChangeCamera)
+        {
+            ResetCounters();
+        }
+    }
+
+
+    private void SwitchCameraByNaviRgationDirection(DirectionType direction)
+    {
+        if (RegisterInput(direction))
+            cameraManager.SwitchFrontAndBackCamera(direction);
+    }
+
+    private void ResetCounters()
+    {
+        time = 0f;
+        currentInputNb = 0;
+    }
+
+    private bool RegisterInput(DirectionType direction)
+    {
+        if (currentInputDirection != direction)
+        {
+            currentInputNb = 0;
+            currentInputDirection = direction;
+        }
+        else if (currentInputNb == 0)
+        {
+            time = 0f;
+        }
+
+        currentInputNb++;
+
+        if (currentInputNb >= inputReqToChangeCamera)
+        {
+            ResetCounters();
+            return true;
+        }
+        return false;
+    }
+}
